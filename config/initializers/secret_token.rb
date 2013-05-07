0f41370c841212cbed0abe16bdafb96fd3ce0a44
Dillon Lara <dillonlara115@fullsail.edu>
2013-05-06 20:55:03 -0500

    initial commit of Social Spin website.  Added bootstrap and basic navigational content.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialSpin::Application.config.secret_token = 'f1448231335c01c4afeaf72a346074d8ab87ae97c6fc93559c758bdce76aae083724931f056f694adf3d6d2ba8a8272e55793ac5eae384fdcc8cf66351dab1b3'
