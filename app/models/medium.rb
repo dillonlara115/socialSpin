078a2f224e73e3ea0d76ad4aa584c6f7ebd02ef1
Dillon Lara <dillonlara115@fullsail.edu>
2013-05-20 19:54:12 -0500

    Revert "tried working with a photo uploader gem but rain into issues."
    This reverts commit 66bf1f7c37dce33c3e26ffc841bf44b8cce3f131.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,19 +1,5 @@
 class Medium < ActiveRecord::Base
-  attr_accessible :album, :album_description, :artist, :artist_description, :average_rating, :year, :arPhoto, :arPhoto_file_name
+  attr_accessible :album, :album_description, :artist, :artist_description, :average_rating, :year
 
   letsrate_rateable "album", "artist"
-
-  has_attached_file :arPhoto,
-  	:styles => {
-    :thumb  => "100x100",
-    :medium => "200x200",
-    :large => "600x400"
-	},
-	:storage => :s3,
-	:s3_credentials => "/config/s3.yml",
-	:path => ":attachment/:id/:style.:extension",
-	:bucket => 'yourbucket'
-
-	# if you are using attr_accessible to protect certain attributes, you will need to allow these:
-	# attr_accessible :photo, :photo_file_name, :photo_content_type, :photo_file_size, :photo_updated_at
 end
