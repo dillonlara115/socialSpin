90fa22ed5c72254c6443b68b39a435a5244ecf32
Dillon Lara <dillonlara115@fullsail.edu>
2013-05-21 20:31:32 -0500

    update photo dimensions and added photos to artists page

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,7 @@
 class Medium < ActiveRecord::Base
   attr_accessible :album, :album_description, :artist, :artist_description, :average_rating, :year, :image
 
+  letsrate_rateable "artist", "album", "song"
+
   mount_uploader :image, ImageUploader
 end
